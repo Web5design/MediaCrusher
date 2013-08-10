ab622c68324ec5d8d470ff795144212ec26eb12c
Drew DeVault <sir@cmpwn.com>
2013-08-10 11:27:59 -0600

    Add submodules, configuration

--- MediaCrusher/Program.cs
+++ MediaCrusher/Program.cs
@@ -1,11 +1,26 @@
 using System;
+using System.IO;
+using Newtonsoft.Json;
+using RedditSharp;
 
 namespace MediaCrusher
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            var config = new Configuration();
+            if (File.Exists("config.json"))
+                config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("config.json"));
+            else
+            {
+                File.WriteAllText("config.json", JsonConvert.SerializeObject(config));
+                Console.WriteLine("Saved empty configuration in config.json, populate it and restart.");
+                return 1;
+            }
+            var reddit = new Reddit();
+            reddit.LogIn(config.Username, config.Password);
+            return 0;
         }
     }
 }
